ad1ccf24401a0b07122ba4c90f42bf9aa7608dc2
Shawn Soneja <shawn.soneja85@gmail.com>
2024-10-16 19:26:46 -0400

    updated to return predictions

--- projects/Linear Regression/main_for_api.py
+++ projects/Linear Regression/main_for_api.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
     logger.info(event)
 
     # Log the event for debugging
-    print(f"Received event: {json.dumps(event)}")
+    # print(f"Received event: {json.dumps(event)}")
 
     # event = json.loads(event)
     data = json.loads(event['body'])
@@ -24,9 +24,20 @@ def lambda_handler(event, context):
         Y = numpy.array(data['data']['y'])
 
         # Print the data to verify
-        print("Data Received:")
-        print(X)
-        print(Y)
+        # print("Data Received:")
+        # print(X)
+        # print(Y)
+
+        # predict, one quarter length of incoming data's time frame, into future. To nearest integer
+        incomingData_X_values = len(X)
+        rangeOfPredictions_length = round(incomingData_X_values/4)
+        rangeOfPredictions_X_values = []
+        for item in range(0,rangeOfPredictions_length):
+            print('item: ', item)
+            x_value = incomingData_X_values + item
+            print('x_value: ', x_value )
+            rangeOfPredictions_X_values.append([x_value])
+            
 
         # Create a Linear Regression model
         model = LinearRegression()
@@ -35,7 +46,11 @@ def lambda_handler(event, context):
         model.fit(X, Y)
 
         # Make predictions
-        Y_pred = model.predict(X)
+        rangeOfPredictions_Y_values = model.predict(rangeOfPredictions_X_values)
+        listOf_Y_Values = rangeOfPredictions_Y_values.tolist()
+
+        print('rangeOfPredictions_X_values: ', rangeOfPredictions_X_values)
+        print('predictions: ', rangeOfPredictions_Y_values)
 
         # Print the model parameters
         print(f"Slope (Coefficient): {model.coef_[0]}")
@@ -44,12 +59,23 @@ def lambda_handler(event, context):
         logger.info("Slope")
         logger.info(model.coef_[0])
 
+        # Success Response
+        response = {
+            'statusCode': 200,
+            'body': json.dumps({
+                'predictions': listOf_Y_Values,
+                'slope': model.coef_[0].tolist(),
+                'errors': []
+            })
+        }
+
     except Exception as e:
             # Error Response, if exception is caught
             response = {
                 'statusCode': 400,
                 'body': json.dumps({
                     'predictions': [],
+                    'slope' : [],
                     'errors': [str(e)]
                 })
             }
@@ -58,15 +84,6 @@ def lambda_handler(event, context):
             logger.info(str(e)  )
 
             return response
-
-    # Success Response
-    response = {
-        'statusCode': 200,
-        'body': json.dumps({
-            'predictions': [],
-            'errors': []
-        })
-    }
 
     return response
 
@@ -79,6 +96,7 @@ LOCAL TESTING SECTION
 with open('../Chat GPT__LTSM Prediction/supporting_files/sample_lambda_request.json') as sample_data:
     data = json.load(sample_data)
 
-lambda_handler(data, "context")
+localResponse = lambda_handler(data, "context")
+print('localResponse: ', localResponse)
 
 """
